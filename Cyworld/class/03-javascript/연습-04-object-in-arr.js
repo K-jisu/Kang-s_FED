e3d48f97ee27048db152da27c378a60fdcfa04f1
KJSoo95 <wltn7star@naver.com>
2022-12-15 23:05:22 +0900

    javascript

---
+++ Cyworld/class/03-javascript/연습-04-object-in-arr.js
@@ -0,0 +1,14 @@
+let classmate = [
+    {name : "철수", age : 13, school : "다람쥐초"},
+    {name : "영희", age : 8, school : "공룡초"},
+    {name : "훈이", age : 11, school : "거북초"}
+    ];
+// undefined
+classmate
+// (3) [{…}, {…}, {…}]0: {name: '철수', age: 13, school: '다람쥐초'}1: {name: '영희', age: 8, school: '공룡초'}2: {name: '훈이', age: 11, school: '거북초'}length: 3[[Prototype]]: Array(0)
+classmate.length
+// 3
+classmate[0].school
+// '다람쥐초'
+classmate[2].age
+// 11
