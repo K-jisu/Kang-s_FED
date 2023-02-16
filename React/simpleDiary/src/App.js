1eb69d3113ebe4e5eadadafe218cd75ae08a4c49
KJSoo95 <wltn7star@naver.com>
2023-02-16 18:03:54 +0900

    -

--- React/simpleDiary/src/App.js
+++ React/simpleDiary/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
@@ -41,15 +41,14 @@ function App() {
     const res = await fetch(
       "https://jsonplaceholder.typicode.com/comments"
     ).then((res) => res.json());
-    console.log(res);
 
     const initData = res.slice(0, 20).map((it) => {
       return {
         author: it.email,
         content: it.body,
         emotion: Math.floor(Math.random() * 5) + 1,
-        created_date : new Date().getTime(),
-        id : datatId.current++
+        created_date: new Date().getTime(),
+        id: datatId.current++,
       };
     });
     setData(initData);
@@ -87,10 +86,28 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    // useMemo 를 쓰면 getDiaryAnalysis에 값을 넣어줌
+    // 그래서 getDiaryAnalysis는 함수가 아니라 값으로 쓰임
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+  // getDiaryAnalysis 를 함수호출이 아니라 그냥 값으로 씀
+
   return (
     <div className="App">
       {/* <LifeCycle/> */}
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length} </div>
+      <div>기분 좋은 일기 개수 : {goodCount} </div>
+      <div>기분 나쁜 일기 개수 : {badCount} </div>
+      <div>기분 좋은 일기 비율 : {goodRatio} </div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
