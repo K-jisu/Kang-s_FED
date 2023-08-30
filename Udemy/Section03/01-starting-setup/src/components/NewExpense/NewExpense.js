0aa03c57b8c472b30093bb0b4ee529d355a718cc
KJSoo95 <wltn7star@naver.com>
2023-08-30 20:16:17 +0900

    Udemy-React

--- Udemy/Section03/01-starting-setup/src/components/NewExpense/NewExpense.js
+++ Udemy/Section03/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -4,9 +4,16 @@ import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
+  const saveExpenesDataHandler = (enteredExpenseData) =>{
+    const expenseData = {
+      ...enteredExpenseData,
+      id : Math.random().toString()
+    };
+    console.log(expenseData)
+  };
   return (
     <div className='new-expense'>
-      <ExpenseForm/>
+      <ExpenseForm onSaveExpenseData = {saveExpenesDataHandler}/>
     </div>
   );
 };
