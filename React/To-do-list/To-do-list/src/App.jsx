8b17250c05ff1e35fd2b6176a38a209c524bf777
KJSoo95 <wltn7star@naver.com>
2024-04-23 22:50:55 +0900

    feat : to do list

---
+++ React/To-do-list/To-do-list/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((cur) => [todo, ...cur]);
+    setTodo("");
+  };
+  console.log(todos);
+  
+  return (
+    <>
+    <h1>My To do ({todos.length}개)</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          placeholder="write your todo"
+          onChange={onChange}
+          type="text"
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
