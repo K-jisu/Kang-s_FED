42e1be1136786d904f9c74b13b14d4e9ae198b9b
KJSoo95 <wltn7star@naver.com>
2024-07-10 20:49:22 +0900

    feat: 인터페이스 함수타입

--- TypeScript/practice/src/Interface.tsx
+++ TypeScript/practice/src/Interface.tsx
@@ -1,23 +1,41 @@
 // import React from "react";
 
 // 인터페이스는 객체의 구조를 정의하고 객체가 인터페이스랑 일치하는지 확인
+// 인터페이스도 상속 가능
 const Interface = () => {
-  interface Person {
-    name: string;
-    age: number;
+  // type AddFn = (a: number, b: number) => number;
+  interface AddFn {
+    (a:number, b:number) : number
+  }
 
+  const add: AddFn = (n1: number, n2: number) => {
+    return n1 + n2;
+  };
+
+  interface Named {
+    readonly name: string;
+  }
+
+  interface Greetable extends Named {
     greet(phrase: string): void;
   }
 
-  const user1: Person = {
-    name: "Max",
-    age: 30,
+  class Person implements Greetable {
+    name: string;
+    age = 30;
 
-    greet(phrase: string) {
+    constructor(n: string) {
+      this.name = n;
+    }
+
+    greet(phrase: string): void {
       console.log(phrase + " " + user1.name);
-    },
-  };
-  user1.greet("Hi my name is ");
+    }
+  }
+
+  const user1: Greetable = new Person("Max");
+  user1.greet("Hi iam");
+  console.log(user1);
 
   return <div></div>;
 };
